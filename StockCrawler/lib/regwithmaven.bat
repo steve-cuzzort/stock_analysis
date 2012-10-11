09cc5f73c039f63bfa874c1ec42846efccf69a0e
steve cuzzort <stephen.cuzzort@gmail.com>
2012-10-10 23:23:44 -0400

    added bat file to register weka

---
+++ StockCrawler/lib/regwithmaven.bat
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=ta-lib.jar -DgroupId=ta-lib -DartifactId=ta-lib -Dversion=0.4.0 -Dpackaging=jar
+mvn install:install-file -Dfile=weka.jar -DgroupId=weka -DartifactId=weka -Dversion=3.6.7 -Dpackaging=jar
